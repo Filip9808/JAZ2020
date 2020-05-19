b52385c0e226942d590a4ad374d7206ab4838d7a
Filip9808 <filipswiatek98@gmail.com>
2020-05-19 18:45:24 +0200

    Zadanie 2 działające po 2 lekcji

--- app/src/main/java/pl/jazapp/app/SecurityFilter.java
+++ app/src/main/java/pl/jazapp/app/SecurityFilter.java
@@ -1,7 +1,5 @@
 package pl.jazapp.app;
-
-
-
+import javax.faces.application.ResourceHandler;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
@@ -9,32 +7,27 @@ import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-
-
 @WebFilter("*")
-public class ExampleFilter extends HttpFilter {
+public class SecurityFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        if (isUserLogged() || isSiteAllowed(req)) {
+        if (isUserLogged() || isSiteAllowed(req) || isResourceReq(req)) {
             chain.doFilter(req, res);
         } else {
             res.sendRedirect(req.getContextPath() + "/login.xhtml");
         }
-
-
-
     }
-
-
-
+    private boolean isUserAuthenticated() {
+        return false;
+    }
+    private boolean isResourceReq(HttpServletRequest req) {
+        return req.getRequestURI().startsWith(
+                req.getContextPath() + ResourceHandler.RESOURCE_IDENTIFIER + "/");
+    }
     private boolean isSiteAllowed(HttpServletRequest req) {
         return "/login.xhtml".equals(req.getServletPath())
                 || "/register.xhtml".equals(req.getServletPath());
     }
-
-
-
     private boolean isUserLogged() {
         return false;
     }
--- app/src/main/java/pl/jazapp/app/webapp/login/LoginController.java
+++ app/src/main/java/pl/jazapp/app/webapp/login/LoginController.java
@@ -3,6 +3,7 @@ package pl.jazapp.app.webapp.login;
 
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 
@@ -12,6 +13,12 @@ import javax.inject.Named;
 public class LoginController {
     public String login(LoginRequest loginRequest) {
         System.out.println(String.format("Tried to log in with username %s and password %s", loginRequest.getUsername(), loginRequest.getPassword()));
+       if(true) {
+           var flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
+           flash.put("error-message", "Username or Password is incorrect.");
+           return "/login.xhtml?faces-redirect=true";
+       }
+
         return "/index.xhtml?faces-redirect=true";
     }
 }
